aae1c328fde5975c8f20c29fd5419b2a291622e8
idinow <we.evangelista@gmail.com>
2023-06-02 16:31:20 -0400

    feat: implementação da tela de login e cadastro de usuário

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import LoginForm from './LoginForm';
 import MyCalendar from './calendar';
 
 function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setUserLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <MyCalendar />
+    <div>
+      {userLoggedIn ? (
+        <div className="App">
+          {/* Renderizar a tela principal aqui após o login */}
+          <MyCalendar />
+        </div>
+      ) : (
+        <div className="App">
+          {/* Renderizar o formulário de login */}
+          <LoginForm onLogin={handleLogin} />
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/LoginForm.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+// import logo from './calendar/calendarLogo.png'
+import './LoginForm.css';
+
+function LoginForm({ onLogin }) {
+  const [showLogin, setShowLogin] = useState(true);
+
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+    onLogin();
+  };
+
+  return (
+    <div className="geral">
+       <div className="container">
+            <h2>{showLogin ? 'Login' : 'Cadastrar Novo Usuário'}</h2>
+            {showLogin ? (
+                <form id="login-form" className="login-form" onSubmit={handleLoginSubmit}>
+                <input type="text" name="username" placeholder="Nome de usuário" required />
+                <br />
+                <input type="password" name="password" placeholder="Senha" required />
+                <br />
+                <input type="submit" value="Entrar" />
+                <p>
+                    Não tem uma conta?{' '}
+                    <span className="toggle-form" onClick={toggleForm}>
+                    Cadastrar novo usuário
+                    </span>
+                </p>
+                </form>
+            ) : (
+                <form id="signup-form" className="login-form">
+                <input type="text" name="name" placeholder="Nome" required />
+                <br />
+                <input type="text" name="username" placeholder="Nome de usuário" required />
+                <br />
+                <input type="password" name="password" placeholder="Senha" required />
+                <br />
+                <input type="submit" value="Cadastrar" />
+                <p>
+                    Já tem uma conta?{' '}
+                    <span className="toggle-form" onClick={toggleForm}>
+                    Fazer login
+                    </span>
+                </p>
+                </form>
+            )}
+        </div>
+    </div>
+  );
+}
+
+export default LoginForm;
